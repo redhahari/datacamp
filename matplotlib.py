15272f77aa0a51ee230a21807c5b096761b7c753
redhahari <redha.hari.w@gmail.com>
2017-09-03 07:27:11 +0700

    Update matplotlib.py

--- matplotlib.py
+++ matplotlib.py
@@ -1,1 +1,20 @@
 
+import matplotlib.pyplot as plt
+
+year = [1950, 1970, 1990, 2010]
+
+pop = [2.519, 3.692, 5.263, 6.972]
+
+# Line Plot
+plt.plot(year,pop)
+
+# Scatter Plot
+plt.scatter(year,pop)
+
+# Convert Axis-X to Logarithmic
+plt.convertx('log')
+  
+  
+# Display the plot
+  plt.show()
+  
